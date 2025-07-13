590eac94a9136d5706038417d5140ca92a53b3cd
dnblank123 <dnblank143@gmail.com>
2025-07-13 09:40:19 +0800

    Revert "enable dynamic default shader"
    This reverts commit 4436826c486dc848640bce2f52002d9d9b40cab4.

--- src/impl.cpp
+++ src/impl.cpp
@@ -360,11 +360,12 @@ HRESULT STDMETHODCALLTYPE ID3D11Device_CreatePixelShader(
         return procs->CreatePixelShader(pDevice, LOW_FS_TERRAIN_SHADER.data(), LOW_FS_TERRAIN_SHADER.size(), pClassLinkage, ppPixelShader);
 #endif
 
-    } else if (simd_equal(DefaultShader, hash) ) {
+    } else if (simd_equal(DefaultShader, hash) && (QualityVal == 2)) {
 #ifdef OLD_SHADERS
         return procs->CreatePixelShader(pDevice, SIMPLIFIED_FS_DEFAULT_OLD_SHADER.data(), SIMPLIFIED_FS_DEFAULT_OLD_SHADER.size(), pClassLinkage, ppPixelShader);
 #else
-        return procs->CreatePixelShader(pDevice, DYNAMIC_FS_DEFAULT_SHADER.data(), DYNAMIC_FS_DEFAULT_SHADER.size(), pClassLinkage, ppPixelShader);
+        return procs->CreatePixelShader(pDevice, SIMPLIFIED_FS_DEFAULT_SHADER.data(), SIMPLIFIED_FS_DEFAULT_SHADER.size(), pClassLinkage, ppPixelShader);
+        // return procs->CreatePixelShader(pDevice, TEST_FS_DEFAULT_SHADER.data(), TEST_FS_DEFAULT_SHADER.size(), pClassLinkage, ppPixelShader);
 #endif
 
     } else if (simd_equal(SphericalShader, hash) && (QualityVal == 2)) {
